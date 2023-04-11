e9eae7809c6da9bbf14b5e4db04bd5cda8b2d164
jj <jotajota1302@hotmail.com>
2023-04-11 16:44:55 +0200

    commit

---
+++ clientrest/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO beca.clientes (dni, nombre,direccion, password,fechaAlta,apellidos, ciudad,nacimiento) VALUES ('12345678A', 'Jose', 'Avenida del campo', '333333', '2023-04-10', 'Jimenez', 'alicante', '1983-02-013');
