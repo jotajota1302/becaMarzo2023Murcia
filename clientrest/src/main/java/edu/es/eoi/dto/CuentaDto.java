af1122446e418bc5fd9d15be2ed2a9a8d38bbfed
jj <jotajota1302@hotmail.com>
2023-04-12 17:09:58 +0200

    commit

---
+++ clientrest/src/main/java/edu/es/eoi/dto/CuentaDto.java
@@ -0,0 +1,20 @@
+package edu.es.eoi.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CuentaDto {
+		
+	private String nombre;
+	
+	private String apellidos;
+	
+	private double saldo;
+	
+	private String banco;
+	
+	private String ciudad;
+
+}
