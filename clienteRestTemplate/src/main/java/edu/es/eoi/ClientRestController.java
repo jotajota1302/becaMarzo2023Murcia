29a1c7392777f777ec49d38f93a1197c9d99dc2a
jj <jotajota1302@hotmail.com>
2023-04-14 15:52:59 +0200

    commit

---
+++ clienteRestTemplate/src/main/java/edu/es/eoi/ClientRestController.java
@@ -0,0 +1,34 @@
+package edu.es.eoi;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/test")
+public class ClientRestController {
+
+	
+	@GetMapping
+	@RequestMapping("/{id}")
+	public BancoDto doSomething(@PathVariable String id) {
+		
+		//recuperar la lista de bancos
+		
+		//como llamo a un servicio web?
+		
+		RestTemplate template= new RestTemplate();
+		
+		BancoDto devolucion = template.getForObject("http://localhost:8082/bancos/".concat(id), BancoDto.class);
+		
+		return devolucion;
+		
+		
+	}
+	
+}
