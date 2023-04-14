664dfbf1d2b2746eacc7cf02f5217e5d7c726b51
jj <jotajota1302@hotmail.com>
2023-04-14 16:06:08 +0200

    commit

--- clienteRestTemplate/src/test/java/edu/es/eoi/ClienteRestTemplateApplicationTests.java
+++ clienteRestTemplate/src/test/java/edu/es/eoi/ClienteRestTemplateApplicationTests.java
@@ -1,13 +1,23 @@
 package edu.es.eoi;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment =WebEnvironment.RANDOM_PORT)
 class ClienteRestTemplateApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		RestTemplate template= new RestTemplate();
+		
+		BancoDto devolucion = template.getForObject("http://localhost:8082/bancos/".concat("2"), BancoDto.class);
+		
+		Assertions.assertEquals("Santander", devolucion.getNombre());
+		
 	}
 
 }
